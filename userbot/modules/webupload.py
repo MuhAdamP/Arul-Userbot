28728a66c6169a9d380ba24fd63ec9500668550e
Adam <101717842+MuhAdamP@users.noreply.github.com>
2022-03-19 02:23:58 +0000

    Update webupload.py

--- userbot/modules/webupload.py
+++ userbot/modules/webupload.py
@@ -10,7 +10,7 @@ from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
 from userbot.utils import edit_or_reply, ayiin_cmd
 
 
-@arul_cmd(
+@ayiin_cmd(
     pattern="webupload ?(.+?|) (?:--)(anonfiles|transfer|filebin|anonymousfiles|megaupload|bayfiles)"
 )
 async def _(event):
