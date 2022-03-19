cedaa89b32dfeff03beb8cff03b3faf6d1fe2937
Adam <101717842+MuhAdamP@users.noreply.github.com>
2022-03-19 00:38:50 +0000

    Update xiaomi.py

--- userbot/modules/xiaomi.py
+++ userbot/modules/xiaomi.py
@@ -9,7 +9,7 @@ from userbot import CMD_HELP
 from userbot.utils import edit_or_reply, ayiin_cmd
 
 
-@ayiin_cmd(pattern="firmware(?: |$)(.*)")
+@arul_cmd(pattern="firmware(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -33,7 +33,7 @@ async def _(event):
             await event.client.forward_messages(event.chat_id, response.message)
 
 
-@ayiin_cmd(pattern="fastboot(?: |$)(.*)")
+@arul_cmd(pattern="fastboot(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -57,7 +57,7 @@ async def _(event):
             await event.client.forward_messages(event.chat_id, response.message)
 
 
-@ayiin_cmd(pattern="recovery(?: |$)(.*)")
+@arul_cmd(pattern="recovery(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -81,7 +81,7 @@ async def _(event):
             await event.client.forward_messages(event.chat_id, response.message)
 
 
-@ayiin_cmd(pattern="pb(?: |$)(.*)")
+@arul_cmd(pattern="pb(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -105,7 +105,7 @@ async def _(event):
             await event.client.forward_messages(event.chat_id, response.message)
 
 
-@ayiin_cmd(pattern="of(?: |$)(.*)")
+@arul_cmd(pattern="of(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -129,7 +129,7 @@ async def _(event):
             await event.client.forward_messages(event.chat_id, response.message)
 
 
-@ayiin_cmd(pattern="eu(?: |$)(.*)")
+@arul_cmd(pattern="eu(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -153,7 +153,7 @@ async def _(event):
             await event.client.forward_messages(event.chat_id, response.message)
 
 
-@ayiin_cmd(pattern="vendor(?: |$)(.*)")
+@arul_cmd(pattern="vendor(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -177,7 +177,7 @@ async def _(event):
             await event.client.forward_messages(event.chat_id, response.message)
 
 
-@ayiin_cmd(pattern="specs(?: |$)(.*)")
+@arul_cmd(pattern="specs(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return
